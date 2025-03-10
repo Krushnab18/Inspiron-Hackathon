72f88cf5e3b6fb365e681a0e3782897de0281d6c
paras1895 <parasxyz1234@gmail.com>
2025-03-10 19:37:43 +0530

    Add files via upload

--- src/assets/components/aichat/aichat.jsx
+++ src/assets/components/aichat/aichat.jsx
@@ -1,17 +1,11 @@
-// run `npx shadcn@latest add "https://r.assistant-ui.com/assistant-modal"`
- 
-import { AssistantRuntimeProvider } from "@assistant-ui/react";
-import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
-import { AssistantModal } from "@/components/assistant-ui/assistant-modal";
- 
-const MyApp = () => {
-  const runtime = useChatRuntime({
-    api: "/api/chat",
-  });
- 
-  return (
-    <AssistantRuntimeProvider runtime={runtime}>
-      <AssistantModal />
-    </AssistantRuntimeProvider>
-  );
-};
+import './aichat.css'
+
+function AIChat() {
+    return (
+        <>
+            <h1>Welcome</h1>
+        </>
+    )
+}
+
+export default AIChat
